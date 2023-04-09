0d287950c0d7b4e869f7445a1977c564c00a4a72
Varun Kargathara <varunkargathara67@gmail.com>
2023-04-09 21:57:33 +0530

    -

--- lib/logostart.dart
+++ lib/logostart.dart
@@ -12,33 +12,35 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 300,
-            left: 70,
-            child: Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/logo2.png',
-                height: 400,
-                width: 400,
+      body: Center(
+        child: Container(
+          height: 308,
+          width: 315,
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  child: Image.asset(
+                    'assets/logo2.png',
+                    height: 177,
+                    width: 250,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Positioned(
-            top: 200,
-            right: 15,
-            child: Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/logo1.png',
-                height: 400,
-                width: 400,
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  child: Image.asset(
+                    'assets/logo1.png',
+                    height: 238,
+                    width: 243,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
